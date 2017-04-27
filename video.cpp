8f09b5222dcac0fe8ba46c7119a15474453a3502
botanrobert <botanrobert@gmail.com>
2017-04-27 19:55:28 +0300

    head detection

--- video.cpp
+++ video.cpp
@@ -17,11 +17,20 @@ int main(void)
 		return 1;
 	}
 
+	CascadeClassifier c;
+	c.load("E:\\Facultate\\Master An1Sem2\\Roboti inteligenti - Oltean M\\OpenCV\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt.xml");
 	namedWindow("camera");
 
+
+	vector <Rect> capete;
 	while (1) {
 
 		video >> a;
+		c.detectMultiScale(a, capete);
+
+		for (int i = 0; i < capete.size(); i++)
+			rectangle(a, capete[i], Scalar(255, 0, 0));
+
 		imshow("camera", a);
 
 		if (waitKey(1) ==  27)//asteptam o milisecunda pentru a se intampla ceva
